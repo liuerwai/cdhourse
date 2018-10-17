fdaa4f9feaa49ab55c5892436e072d8a213adc2e
刘循光 <liuxg@channelsoft.com>
2018-10-17 17:52:18 +0800

    修改代码

--- src/com/cdhouse/controller/Controller.java
+++ src/com/cdhouse/controller/Controller.java
@@ -1,16 +1,20 @@
 package com.cdhouse.controller;
 
+import com.cdhouse.contans.AreaType;
+import com.cdhouse.contans.HourseType;
 import com.cdhouse.po.DealPo;
 import com.cdhouse.po.PreSalePo;
 import com.cdhouse.po.ResultPo;
+import com.cdhouse.utils.LoggerUtils;
 import com.cdhouse.webservice.IService;
+import org.jsoup.select.Collector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import com.cdhouse.utils.LoggerUtils;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -22,25 +26,80 @@ public class Controller {
     @RequestMapping("/querySum")
     public ResultPo query(String startTime, String endTime) throws IOException {
 
-        LoggerUtils.info("查询数量汇总信息 startTime :" + startTime + "endTime" + endTime) ;
-        return  ResultPo.success(sumService.querySum(startTime, endTime));
+        LoggerUtils.info("查询数量汇总信息 startTime :" + startTime + "endTime" + endTime);
+        return ResultPo.success(sumService.querySum(startTime, endTime));
     }
 
     @ResponseBody
     @RequestMapping("/queryPreSaleInfo")
     public ResultPo queryPreSaleInfo(String startTime, String endTime) throws IOException {
 
-        LoggerUtils.info("查询预售信息 startTime :" + startTime + "endTime" + endTime) ;
+        LoggerUtils.info("查询预售信息 startTime :" + startTime + "endTime" + endTime);
         List<PreSalePo> list = sumService.queryPreSaleInfo(startTime, endTime);
-        return  ResultPo.success(list, list.size(), list.size());
+        return ResultPo.success(list, list.size(), list.size());
     }
 
     @ResponseBody
     @RequestMapping("/queryDealInfo")
     public ResultPo queryDealInfo(String startTime, String endTime) throws IOException {
 
-        LoggerUtils.info("查询交易信息 startTime :" + startTime + "endTime" + endTime) ;
+        LoggerUtils.info("查询交易信息 startTime :" + startTime + "endTime" + endTime);
         List<DealPo> list = sumService.queryDealInfo(startTime, endTime);
-        return  ResultPo.success(list, list.size(), list.size());
+        return ResultPo.success(list, list.size(), list.size());
+    }
+
+    @ResponseBody
+    @RequestMapping("/queryDealInfoForChartLine")
+    public ResultPo queryDealInfoForChartLine(String startTime, String endTime) throws IOException {
+
+        LoggerUtils.info("查询交易信息 startTime :" + startTime + "endTime" + endTime);
+        List<DealPo> dealList = sumService.queryDealInfo(startTime, endTime);
+        Map<String, List> result = new HashMap<String, List>();
+        result.put("X", new ArrayList());
+        result.put("newHourse", new ArrayList());
+        result.put("secondHourse", new ArrayList());
+        result.put("newHourseMainCity", new ArrayList());
+        result.put("secondHourseMainCity", new ArrayList());
+        result.put("newHourseDistantCity", new ArrayList());
+        result.put("secondHourseDistantCity", new ArrayList());
+
+        Map map1 = dealList.stream().filter(dealPo -> dealPo.getType().equals(HourseType.NEW.value))
+                .collect(Collectors.groupingBy(DealPo::getTimeStr, Collectors.summingInt(DealPo::getNum)));
+
+        Map map2 = dealList.stream().filter(dealPo -> dealPo.getType().equals(HourseType.SECOEND.value))
+                .collect(Collectors.groupingBy(DealPo::getTimeStr, Collectors.summingInt(DealPo::getNum)));
+
+        Map map3 = dealList.stream().filter(dealPo -> dealPo.getType().equals(HourseType.NEW.value))
+                .filter(dealPo -> dealPo.getAreaType().equals(AreaType.MAIN_CITY.value))
+                .collect(Collectors.groupingBy(DealPo::getTimeStr, Collectors.summingInt(DealPo::getNum)));
+
+        Map map4 = dealList.stream().filter(dealPo -> dealPo.getType().equals(HourseType.NEW.value))
+                .filter(dealPo -> dealPo.getAreaType().equals(AreaType.DISTANT_CITY.value))
+                .collect(Collectors.groupingBy(DealPo::getTimeStr, Collectors.summingInt(DealPo::getNum)));
+
+        Map map5 = dealList.stream().filter(dealPo -> dealPo.getType().equals(HourseType.SECOEND.value))
+                .filter(dealPo -> dealPo.getAreaType().equals(AreaType.MAIN_CITY.value))
+                .collect(Collectors.groupingBy(DealPo::getTimeStr, Collectors.summingInt(DealPo::getNum)));
+
+        Map map6 = dealList.stream().filter(dealPo -> dealPo.getType().equals(HourseType.SECOEND.value))
+                .filter(dealPo -> dealPo.getAreaType().equals(AreaType.DISTANT_CITY.value))
+                .collect(Collectors.groupingBy(DealPo::getTimeStr, Collectors.summingInt(DealPo::getNum)));
+
+        List<String> list = dealList.stream()
+                .sorted(Comparator.comparing(DealPo::getTimeStr))
+                .map(DealPo::getTimeStr)
+                .distinct()
+                .collect(Collectors.toList());
+        for(String item : list){
+            result.get("X").add(item);
+            result.get("newHourse").add(map1.get(item));
+            result.get("secondHourse").add(map2.get(item));
+            result.get("newHourseMainCity").add(map3.get(item));
+            result.get("secondHourseMainCity").add(map4.get(item));
+            result.get("newHourseDistantCity").add(map5.get(item));
+            result.get("secondHourseDistantCity").add(map6.get(item));
+        }
+
+        return ResultPo.success(result);
     }
 }
--- src/com/cdhouse/test/Test.java
+++ src/com/cdhouse/test/Test.java
@@ -12,6 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -19,6 +24,9 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @ContextConfiguration(classes = RootConfig.class)
 //@Transactional
 public class Test {
+
+    @Autowired
+    WebApplicationContext webAppConfiguration;
 
     @Autowired
     Controller sumController;
@@ -32,26 +40,25 @@ public class Test {
     @Autowired
     IPreSaleDao preSaleDao;
 
-//    @org.junit.Test
-//    public void test() throws Exception{
-//
-//        String url = "/querySum";
-//        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(sumController).build();
-//        String content = mockMvc.perform(MockMvcRequestBuilders.get(url))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andReturn().getResponse().getContentAsString();
-//        System.out.println(content);
-//    }
-
     @org.junit.Test
     public void test() throws Exception{
-        dataService.crawelDealInfo();
-        dataService.crawelPreSaleInfo();
-        dealDao.queryDealInfo("2017-07-01", "2018-12-12");
-        dealDao.querySum("2017-07-01", "2018-12-12");
-        preSaleDao.queryPreSale("2017-07-01", "2018-12-12");
-        preSaleDao.querySum("2017-07-01", "2018-12-12");
 
+        String url = "/queryDealInfoForChartLine";
+        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webAppConfiguration).build();
+        String content = mockMvc.perform(MockMvcRequestBuilders.get(url))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString();
+        System.out.println(content);
     }
+
+//    @org.junit.Test
+//    public void test() throws Exception{
+//        dataService.crawelDealInfo();
+//        dataService.crawelPreSaleInfo();
+//        dealDao.queryDealInfo("2017-07-01", "2018-12-12");
+//        dealDao.querySum("2017-07-01", "2018-12-12");
+//        preSaleDao.queryPreSale("2017-07-01", "2018-12-12");
+//        preSaleDao.querySum("2017-07-01", "2018-12-12");
+//    }
 
 }
